a6681c6636083554a79c0a54bce2e171547562bb
婞玦 <132133021+linda7511@users.noreply.github.com>
2024-06-19 11:25:07 +0800

    bug分支 工具部分更新

--- src/views/project/UnitTesting.vue
+++ src/views/project/UnitTesting.vue
@@ -1,33 +1,112 @@
 <template>
-  <div>
-    <button @click="runTests">Run Tests</button>
-    <div v-if="testResults">...Display test results here...</div>
+  <div class="flex-container">
+    <!-- 子系统选择下拉框 -->
+    <label for="subsystemSelect">选择子系统：</label>
+    <select name="subsystemSelect" id="subsystemSelect" v-model="selectedSubsystem" @change="updateClasses">
+      <option value="" disabled>请选择子系统</option>
+      <option v-for="subsystem in subsystemList" :key="subsystem" :value="subsystem">
+        {{ subsystem }}
+      </option>
+    </select>
+
+    <!-- 类选择下拉框，根据子系统动态更新 -->
+    <label for="classSelect">选择类：</label>
+    <select id="classSelect" v-model="selectedClass" @change="updateMethods">
+      <option value="" disabled>请选择类</option>
+      <option v-for="classItem in filteredClassList" :key="classItem" :value="classItem">
+        {{ classItem }}
+      </option>
+    </select>
+
+    <!-- 方法选择下拉多选框，根据类动态更新 -->
+    <label for="methodSelect">选择方法：</label>
+    <select id="methodSelect" v-model="selectedMethods">
+      <option value="" disabled>请选择方法</option>
+      <option v-for="method in filteredMethodsList" :key="method" :value="method">
+        {{ method }}
+      </option>
+    </select>
     <div>
-      <iframe v-if="iframeShow" :src="'2024STproject/SE_Back_End/canteen-service/target/site/allure-maven-plugin/index.html'" width="100%" height="500px"></iframe>
+      <button @click="runTests">Run Tests</button>
+    </div>
+  </div>
+  <div>
+    <!-- 加载动画，这里用v-show替代v-if以实现过渡效果 -->
+    <div v-show="loading">...Loading...
+      <el-icon class="error-icon"><Loading /></el-icon>
+    </div>
+    <div>
+      <iframe v-if="iframeShow" :src="'2024STproject/SE_Back_End/canteen-service/target/allure-report/index.html'" width="100%" height="600px"></iframe>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+// import { Loading } from '@element-plus/icons-vue';
 
 export default {
+  // components:{
+  //   Loading
+  // },
   data() {
     return {
+      // 初始数据
+      selectedSubsystem: null,
+      selectedClass: null,
+      selectedMethods: [],
+      subsystemList:['餐厅子系统','人事子系统','绩效子系统','前台接待子系统'],
+      classList: {
+        '餐厅子系统': ['Dish', 'Book','MyOrder','MyTable'],
+        '人事子系统': ['Class3', 'Class4'],
+        '绩效子系统': ['Class3', 'Class4'],
+        '前台接待子系统': ['Class3', 'Class4'],
+      },
+      methodList: {
+        'Dish': ['newDish', 'removeDish','updateDish','all'],
+        'Book': ['newBookInfo', 'modifyBookInfo', 'cancleBookInfo','all'],
+        'MyOrder': ['order', 'modifyMyOrder','all'],
+        'MyTable': ['createTable', 'amendTable', 'removeBookInfo','all'],
+      },
       testResults: null,
-      iframeShow: false,
+      iframeShow: true,//false
+      loading: false,
+      iframeSrc: '' // 初始为空，根据需要更新
     };
   },
+  computed: {
+    filteredClassList() {
+      return this.selectedSubsystem && this.classList[this.selectedSubsystem] || [];    },
+    filteredMethodsList() {
+      return this.selectedClass && this.methodList[this.selectedClass] || [];    }
+  },
   methods: {
+    updateClasses() {
+      console.log('updateClasses called');
+      if (this.selectedSubsystem) {
+        this.selectedClass = this.classList[this.selectedSubsystem][0]; // 默认选择第一个类
+      } else {
+        this.selectedClass = null;
+      }
+      this.selectedMethods = [];
+    },
+    updateMethods() {
+      console.log('updateMethods called');
+      if (this.selectedClass) {
+        this.selectedMethods = this.methodList[this.selectedClass] ? [this.methodList[this.selectedClass][0]] : []; // 默认选择第一个方法
+      }
+    },
     async runTests() {
+      this.loading = true;
       console.log("run start");
       try {
-        const response = await axios.post('http://localhost:3000/run-tests');
+        const response = await axios.post('http://localhost:3000/run-unitTests');
         console.log("response:",response);
 
         const reportHtml = response.data.reportHtml;
         console.log('reportHtml:',reportHtml);
         //this.embedReportInFrontend(reportHtml);
+        this.loading = false;
         this.iframeShow = true;
 
         console.log('Allure report embedded in frontend.');
@@ -46,3 +125,25 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.flex-container {
+  display: flex;          /* 启用Flexbox布局 */
+  justify-content: space-around; /* 项目均匀分布在容器内 */
+  align-items: center;    /* 垂直居中对齐项目 */
+  flex-wrap: wrap;        /* 允许换行 */
+}
+
+.flex-container select {
+  margin: 5px;            /* 添加一些外边距 */
+  flex: 1;                /* 让每个下拉框占据可用空间 */
+  min-width: 100px;       /* 设置最小宽度，防止过于拥挤 */
+  max-width: 200px;       /* 设置最大宽度，控制下拉框大小 */
+}
+.error-icon {
+    position: absolute;
+    left: 42%;
+    top: 60%;
+    color: #818791;
+}
+</style>
--- unitTestServer.js
+++ unitTestServer.js
@@ -9,45 +9,211 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.post('/run-tests', (req, res) => {
+app.post('/run-unitTests', (req, res) => {
     console.log('Current working directory:', process.cwd());
 
-    console.log('Received request to run tests');
+    console.log('Received request to run unit tests');
 
     const projectPath = path.join(__dirname, '2024STproject/SE_Back_End/canteen-service');
 
-    console.log('projectPath:',projectPath);
-    exec('mvn clean test', { cwd: projectPath },(error, stdout, stderr) => {
-        if (error) {
-        console.error('Error during mvn clean test:', error);
-        return res.status(500).json({ error: 'Error running tests' });
-    }
-    
-    console.log('mvn clean test stdout:', stdout);
-    console.error('mvn clean test stderr:', stderr);
+    // const allureResultsPath = path.join(projectPath, 'allure-results');
+  
+    // // 运行 shell 脚本来管理历史数据
+    // exec('path/to/manage_allure_history.sh', (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error('Error managing Allure history:', error);
+    //     reject(error);
+    //     return;
+    //   }
+    //   console.log('stdout:', stdout);
+    //   console.error('stderr:', stderr);
+    //   resolve();
+    // });
 
-    exec('mvn allure:report', { cwd: projectPath }, (error, stdout, stderr) => {
-      if (error) {
-        console.error('Error during mvn allure:report:', error);
-        return res.status(500).json({ error: 'Error generating report' });
+    console.log('projectPath:',projectPath);
+    exec('mvn clean test -Dtest=com.jike.canteen.service.impl.**', { cwd: projectPath },(error, stdout, stderr) => {
+        if (error) {
+            console.error('Error during mvn clean test:', error);
+            return res.status(500).json({ error: 'Error running tests' });
+        }
+    
+        console.log('mvn clean test stdout:', stdout);
+        console.error('mvn clean test stderr:', stderr);
+
+        exec('mvn allure:report', { cwd: projectPath }, (error, stdout, stderr) => {
+          if (error) {
+            console.error('Error during mvn allure:report:', error);
+            return res.status(500).json({ error: 'Error generating report' });
+          }
+
+          console.log('mvn allure:report stdout:', stdout);
+          console.error('mvn allure:report stderr:', stderr);
+
+          const allureReportPath = path.join(__dirname, '2024STproject/SE_Back_End/canteen-service', 'target', 'allure-report', 'index.html');
+
+          fs.readFile(allureReportPath, 'utf8', (readError, data) => {
+            if (readError) {
+              console.error('Error reading report file:', readError);
+              return res.status(500).json({ error: 'Error reading report file' });
+            }
+
+            console.log('reportHtml:', data);
+            res.json({ reportHtml: data });
+          });
+        });
+      });
+});
+
+app.post('/run-integrationTests', (req, res) => {
+  console.log('Current working directory:', process.cwd());
+
+  console.log('Received request to run integration tests');
+
+  // 定义模块路径数组
+  const modules = ['canteen-service', 'frontdesk-service', 'performance-service', 'personnel-service'];
+  const projectPath = path.join(__dirname, '2024STproject/SE_Back_End');
+  //const allureResultsPath = path.join(projectPath, 'allure-results');
+
+  console.log('projectPath:',projectPath);
+  exec('mvn allure:report', { cwd: projectPath }, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error during mvn allure:report:', error);
+      return res.status(500).json({ error: 'Error generating report' });
+    }
+
+    console.log('mvn allure:report stdout:', stdout);
+    console.error('mvn allure:report stderr:', stderr);
+
+    const allureReportPath = path.join(__dirname, '2024STproject/SE_Back_End', 'target', 'allure-report', 'index.html');
+
+    fs.readFile(allureReportPath, 'utf8', (readError, data) => {
+      if (readError) {
+        console.error('Error reading report file:', readError);
+        return res.status(500).json({ error: 'Error reading report file' });
       }
 
-      console.log('mvn allure:report stdout:', stdout);
-      console.error('mvn allure:report stderr:', stderr);
-
-      const allureReportPath = path.join(__dirname, '2024STproject/SE_Back_End/canteen-service', 'target', 'site', 'allure-maven-plugin', 'index.html');
-
-      fs.readFile(allureReportPath, 'utf8', (readError, data) => {
-        if (readError) {
-          console.error('Error reading report file:', readError);
-          return res.status(500).json({ error: 'Error reading report file' });
-        }
-
-        console.log('reportHtml:', data);
-        res.json({ reportHtml: data });
-      });
+      console.log('reportHtml:', data);
+      res.json({ reportHtml: data });
     });
   });
+    // exec('mvn clean test', { cwd: projectPath },(error, stdout, stderr) => {
+    //     if (error) {
+    //         console.error('Error during mvn clean test:', error);
+    //         return res.status(500).json({ error: 'Error running tests' });
+    //     }
+    
+    //     console.log('mvn clean test stdout:', stdout);
+    //     console.error('mvn clean test stderr:', stderr);
+
+    //     exec('mvn allure:report', { cwd: projectPath }, (error, stdout, stderr) => {
+    //       if (error) {
+    //         console.error('Error during mvn allure:report:', error);
+    //         return res.status(500).json({ error: 'Error generating report' });
+    //       }
+
+    //       console.log('mvn allure:report stdout:', stdout);
+    //       console.error('mvn allure:report stderr:', stderr);
+
+    //       const allureReportPath = path.join(__dirname, '2024STproject/SE_Back_End', 'target', 'allure-report', 'index.html');
+
+    //       fs.readFile(allureReportPath, 'utf8', (readError, data) => {
+    //         if (readError) {
+    //           console.error('Error reading report file:', readError);
+    //           return res.status(500).json({ error: 'Error reading report file' });
+    //         }
+
+    //         console.log('reportHtml:', data);
+    //         res.json({ reportHtml: data });
+    //       });
+    //     });
+    //   });
+
+  // 确保allure-results目录存在
+  // fs.mkdir(allureResultsPath, { recursive: true }, (err) => {
+  //   if (err) {
+  //     console.error('Error creating allure-results directory:', err);
+  //     return res.status(500).json({ error: 'Error creating results directory' });
+  //   }
+
+  //   // 运行每个模块的测试并收集结果
+  //   let testsRunning = true;
+  //   let finishedModules = 0;
+
+  //   // modules.forEach(moduleName => {
+  //   //   const modulePath = path.join(projectPath, moduleName);
+  //   //   console.log(`Running tests for module: ${moduleName}`);
+
+  //   //   exec('mvn test', { cwd: modulePath }, (error, stdout, stderr) => {
+  //   //     if (error) {
+  //   //       console.error(`Error during mvn clean test for ${moduleName}:`, error);
+  //   //       console.log(`mvn clean test stdout for ${moduleName}:`, stdout);
+  //   //       console.error(`mvn clean test stderr for ${moduleName}:`, stderr);
+  //   //       testsRunning = false;
+  //   //       return;
+  //   //     }
+
+  //   //     console.log(`mvn clean test stdout for ${moduleName}:`, stdout);
+  //   //     console.error(`mvn clean test stderr for ${moduleName}:`, stderr);
+
+  //   //     // 收集Allure结果到allure-results目录
+  //   //     const allureResultsModulePath = path.join(modulePath, 'target', 'allure-report');
+  //   //     const allureResultsDestPath = path.join(allureResultsPath, moduleName);
+
+  //   //     // 确保模块的allure结果目录存在
+  //   //     fs.mkdir(allureResultsDestPath, { recursive: true }, (err) => {
+  //   //       if (err) {
+  //   //         console.error('Error creating module allure-results directory:', err);
+  //   //         return;
+  //   //       }
+
+  //   //       // 复制Allure结果到allure-results目录
+  //   //       fs.copy(allureResultsModulePath, allureResultsDestPath, (err) => {
+  //   //         if (err) {
+  //   //           console.error('Error copying allure results:', err);
+  //   //           return;
+  //   //         }
+  //   //         console.log(`Allure results copied for module: ${moduleName}`);
+  //   //         finishedModules++;
+
+  //   //         // 检查是否所有模块的测试都已完成
+  //   //         if (finishedModules === modules.length) {
+  //   //           generateAllureReport();
+  //   //         }
+  //   //       });
+  //   //     });
+  //   //   });
+  //   // });
+
+  //   // 生成最终的Allure报告
+    
+  //   function generateAllureReport() {
+  //     if (!testsRunning) {
+  //       return res.status(500).json({ error: 'Some tests failed' });
+  //     }
+
+  //     console.log('Generating Allure report...');
+  //     exec('mvn allure:serve -Dallure.results.directory=allure-results', { cwd: projectPath }, (error, stdout, stderr) => {
+  //       if (error) {
+  //         console.error('Error during mvn allure:serve:', error);
+  //         return res.status(500).json({ error: 'Error generating report' });
+  //       }
+
+  //       console.log('mvn allure:serve stdout:', stdout);
+  //       console.error('mvn allure:serve stderr:', stderr);
+
+  //       const allureReportPath = path.join(projectPath, 'allure-report', 'index.html');
+  //       fs.readFile(allureReportPath, 'utf8', (readError, data) => {
+  //         if (readError) {
+  //           console.error('Error reading report file:', readError);
+  //           return res.status(500).json({ error: 'Error reading report file' });
+  //         }
+
+  //         console.log('reportHtml:', data);
+  //         res.json({ reportHtml: data });
+  //       });
+  //     });
+  //   }
+  // });
 });
 
 app.listen(port, () => {
